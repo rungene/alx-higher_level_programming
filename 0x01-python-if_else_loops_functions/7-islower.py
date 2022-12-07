a21ec7c901c08c1607d4dc54435940f955ef03ab
rungene <rungenesamuel@gmail.com>
2022-12-06 22:09:57 -0800

    Corrections

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,5 +2,7 @@
 def islower(c):
     if ord('c') >= 97 and ord('c') <= 122:
         return True
+        print("{} is lower".format(c))
     elif:
         return False
+        print("{} is upper".format(c))
