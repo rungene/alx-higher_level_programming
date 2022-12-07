fd438a643b0a37c8a052ee6b82dcc955797f3fbf
rungene <rungenesamuel@gmail.com>
2022-12-07 12:11:33 -0800

    Corrections

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,4 +5,4 @@ for i in range(0, 9):
         if i == 8:
             print("{:d}{:d}".format(i, j))
             break
-        print("{:d}{:d}".format(i, j), end=",")
+        print("{:d}{:d}".format(i, j), end=", ")
