d7fcccda2ba0cfbc7dc7628b107872cb7534fce2
rungene <rungenesamuel@gmail.com>
2022-12-13 06:40:57 -0800

    Adding a check to make sure it is a list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,5 +4,6 @@ def print_reversed_list_integer(my_list=[]):
     prints all integers of a list, in
     reverse order
     """
-    for i in reversed(my_list):
-        print("{:d}".format(i))
+    if type(my_list) is list:
+        for i in reversed(my_list):
+            print("{:d}".format(i))
