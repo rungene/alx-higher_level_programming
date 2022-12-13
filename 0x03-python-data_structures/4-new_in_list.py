794e07c0d30e84c067b168b48e1ec3ef77c4ba96
rungene <rungenesamuel@gmail.com>
2022-12-13 08:00:39 -0800

    Fixing pycode style errors

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 def new_in_list(my_list, idx, element):
     """
     A function that replaces an element
-    in a list at a specific position 
+    in a list at a specific position
     without modifying the original list
     """
     if idx < 0:
