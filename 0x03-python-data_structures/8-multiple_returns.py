4b43e4ec410e935df8f1066c3ad70f03a7cd6825
rungene <rungenesamuel@gmail.com>
2022-12-18 03:34:02 -0800

    Corrections

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,9 +5,9 @@ def multiple_returns(sentence):
     a function that returns a tuple with the
     length of a string and its first character.
     """
-    if len(sentence) == 0:
+    strlen = len(sentence)
+    if strlen == 0:
         first = None
     else:
-        length = len(sentence)
         first = sentence[0]
-    return length, first
+    return (strlen, first)
