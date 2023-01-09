0a9d081c2809d351d31645aa2cd8a90ec6a95fc1
rungene <rungenesamuel@gmail.com>
2023-01-09 01:05:24 -0800

    a function that returns a new dictionary with all values multiplied by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    """
+    a function that returns a new dictionary
+    with all values multiplied by 2
+    """
+    b_dict = dict(a_dictionary)
+    for ke, val in sorted(b_dict.items()):
+            b_dict[ke] = val * 2
+    return b_dict
