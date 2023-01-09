e609d70ac42a81a24e8d9775bfb3a56e699d14c8
rungene <rungenesamuel@gmail.com>
2023-01-09 01:30:04 -0800

    Correcting pycodestyle errors

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -7,5 +7,5 @@ def multiply_by_2(a_dictionary):
     """
     b_dict = dict(a_dictionary)
     for ke, val in sorted(b_dict.items()):
-            b_dict[ke] = val * 2
+        b_dict[ke] = val * 2
     return b_dict
