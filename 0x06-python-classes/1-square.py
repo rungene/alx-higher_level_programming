61e483a831192a93446fbe1c380fad8f1711df0c
rungene <rungenesamuel@gmail.com>
2023-01-14 03:04:57 -0800

    Write a class Square that defines a square by Private instance attribute: size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module demonstrates an empty class Square that defines a square:
+without  import any module
+"""
+
+
+class Square:
+    """
+    empty class Square that defines a square
+    Attributes:
+        none
+    """
+    def __init__(self, size=None):
+        """
+        the __init__ method for square class
+        Args:
+            size:private instance size of a square.
+        """
+        self.is_new = True
+        if size is not None:
+            self.__size = size
