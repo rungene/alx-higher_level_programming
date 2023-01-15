04aabb126b5e2f1aa38837f03695beb0ae40dec7
rungene <rungenesamuel@gmail.com>
2023-01-15 01:14:36 -0800

    Updating the class doc strings

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-This module demonstrates an empty class Square that defines a square:
-without  import any module
+This module demonstrates how to calculate Area of a square 
 """
 
 
