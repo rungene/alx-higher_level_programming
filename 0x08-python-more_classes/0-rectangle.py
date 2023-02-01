e78b309ec78d04e1bcbf9202e9dce90d1c7d9649
rungene <rungenesamuel@gmail.com>
2023-02-01 09:02:57 -0800

    an empty class Rectangle that defines a rectangle:

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on 1.2.23
+@author: Rungene
+"""
+
+
+class Rectangle:
+    """
+     an empty class Rectangle that defines a rectangle
+
+     Attributes:
+        None
+     """
+    pass
