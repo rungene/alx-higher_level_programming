11efb547921525301164658a4ae792be888d5b97
rungene <rungenesamuel@gmail.com>
2023-02-07 18:48:00 -0800

    a class BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Created on Tuesday 7.02.2023
+@author: Rungene
+"""
+
+
+class BaseGeometry():
+    """
+    calculates the area
+
+    Attributes:
+        None
+    """
+    def __init__(self):
+        """
+        init method
+        """
+    def area(self):
+        """
+        Public instance method area
+
+        Raises:
+            Exception("area() is not implemented")
+        """
+        raise Exception("area() is not implemented")
