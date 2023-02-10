b76610598ca549aefe182f05e90840b3e0807dda
rungene <rungenesamuel@gmail.com>
2023-02-10 05:05:56 -0800

    a function that writes an Object to a text file, using a JSON representation:

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Created on Friday 10.02.2023
+@author: Rungene
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    writes an Object to a text file,
+    using a JSON representation:
+    
+    Attributes:
+        my_obj - python object
+        filename - json file to write to
+
+    Return:
+        file in json format
+    """
+    json_string = json.dumps(my_obj)
+    with open(filename, 'w', encoding='utf-8') as a_file:
+        return (a_file.write(json_string))
