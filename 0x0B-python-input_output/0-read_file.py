5d9c513641955f44079c271de3c5d57edfa4afa7
rungene <rungenesamuel@gmail.com>
2023-02-10 01:09:55 -0800

    Corrections, pycodestyle error

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,7 +8,7 @@ Created on Friday 10.02.2023
 def read_file(filename=""):
     """
     function that reads a text file (UTF8) and prints it to stdout:
-    
+
     Attrubutes:
         filename(str):the name of the file to open
     """
