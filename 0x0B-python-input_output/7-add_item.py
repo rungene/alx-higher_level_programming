ed3c2dcc48fba250b72e2426d63ae402cad53d8a
rungene <rungenesamuel@gmail.com>
2023-02-10 07:29:52 -0800

    a script that adds all arguments to a Python list, and then save them to a file:

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Created on Friday 10.02.2023
+@author: Rungene
+"""
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = sys.argv
+file_name = 'add_item.json'
+
+try:
+    list_add = load_from_json_file(file_name)
+except:
+    list_add = []
+
+for i in range(1, len(args)):
+    list_add.append(args[i])
+
+try:
+    save_to_json_file(list_add, file_name)
+except:
+    pass
