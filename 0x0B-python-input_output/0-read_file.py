ecbdfba20bb38e5f551fb0f7d0b4323c9139b49d
rungene <rungenesamuel@gmail.com>
2023-02-10 01:07:12 -0800

    Corrections, printing format. As well as docstring

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,7 +8,10 @@ Created on Friday 10.02.2023
 def read_file(filename=""):
     """
     function that reads a text file (UTF8) and prints it to stdout:
+    
+    Attrubutes:
+        filename(str):the name of the file to open
     """
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data)
+        print(read_data, end="")
