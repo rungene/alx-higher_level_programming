b99ea1b88f1639dff2f1ac00f99133fccd105fa2
rungene <rungenesamuel@gmail.com>
2023-02-12 22:45:30 -0800

    Adding a base class, Base class . Task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Base class module
+Created on Monday 13.02.2023
+@author: Rungene
+"""
+
+
+class Base:
+    """
+    Base class
+
+    Attributes:
+       private int __nb_objects
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        __init__ method for initializing instance variables
+
+        Attributes:
+            id integer input for for id
+        """
+        if id not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
