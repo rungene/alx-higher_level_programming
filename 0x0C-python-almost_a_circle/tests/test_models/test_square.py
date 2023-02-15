3e04445e8e15c1c12a2eb8b55226019096c3f715
rungene <rungenesamuel@gmail.com>
2023-02-15 08:19:06 -0800

    Adding test for input values

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 test_square unittest module
-Created on Monday 15.02.2023
+Created on Wed 15.02.2023
 @author: Rungene
 """
 
@@ -16,7 +16,7 @@ from contextlib import redirect_stdout
 
 class TestSquare(unittest.TestCase):
     """"
-    TestSquare - class for testing Rectangle class methods
+    TestSquare - class for testing Square class methods
     """
     @classmethod
     def setUpClass(cls):
@@ -36,7 +36,7 @@ class TestSquare(unittest.TestCase):
 
     def test_module_test_square_conform_pep8(self):
         """
-        Test test_square module  conforms to PEP8
+        Test test_square.py conforms to PEP8
         """
         pep8_code_style = pep8.StyleGuide(quiet=True)
         result = pep8_code_style.check_files(
@@ -63,3 +63,119 @@ class TestSquare(unittest.TestCase):
         """
         for doc_mthds in self.setup:
             self.assertTrue(len(doc_mthds[1].__doc__) >= 1)
+
+    def test_expected_values(self):
+        """
+        Testing expected Normal values just for width and height
+        x, y and id
+        """
+        s0 = Square(1, 2)
+        s1 = Square(1, 2, 3)
+        s2 = Square(1, 2, 3, 4)
+        self.assertEqual([s0.size, s0.x, s0.y, s0.id],
+                         [1, 2, 0, 1])
+        self.assertEqual([s1.size, s1.x, s1.y, s1.id],
+                         [1, 2, 3, 2])
+        self.assertEqual([s2.size, s2.x, s2.y, s2.id],
+                         [1, 2, 3, 4])
+
+    def test_wrong_input_vals(self):
+        """
+        Testing for zero and negative values
+        """
+        with self.assertRaises(ValueError):
+            R = Rectangle(0, 0)
+        with self.assertRaises(ValueError):
+            R = Rectangle(-1, -1)
+        with self.assertRaises(ValueError):
+            R = Rectangle(1, 1, -1, -1)
+        with self.assertRaises(TypeError):
+            R = Rectangle()
+        with self.assertRaises(TypeError):
+            R = Rectangle(1, 2, 3, 4, 5, 6, 7)
+
+    def test_input_types(self):
+        """
+        Testing for different input types
+        Raise exception where necessary
+        """
+        with self.assertRaises(TypeError):
+            R = Rectangle('height', 'width')
+        with self.assertRaises(TypeError):
+            R = Rectangle(1.2, 1.3)
+        with self.assertRaises(TypeError):
+            R = Rectangle(1, 1, 'x', 'y')
+        with self.assertRaises(TypeError):
+            R = Rectangle(1, 2, 2.3, 1.3)
+        with self.assertRaises(TypeError):
+            R = Rectangle(True, False)
+        with self.assertRaises(TypeError):
+            R = Rectangle(1, 2, True, False)
+        with self.assertRaises(TypeError):
+            R = Rectangle([1, 2], 3, 4, 5)
+        with self.assertRaises(TypeError):
+            R = Rectangle((1, 2), 3, 4, 5)
+        with self.assertRaises(TypeError):
+            R = Rectangle((1, 2), 'x', 'y')
+        with self.assertRaises(TypeError):
+            R = Rectangle({1: 1}, 2, 3, 4)
+        with self.assertRaises(TypeError):
+            R = Rectangle({1: 2, 2: 3}, 5, 6)
+
+    def test_area_mthd(self):
+        """"
+        Testing the area method
+        """
+        R = Rectangle(10, 10)
+        self.assertEqual(R.area(), 100)
+        with self.assertRaises(TypeError):
+            A = R.area(1)
+
+    def test_str_mthd(self):
+        """
+        Testing the str method returns the corret formated string
+        """
+        r_string = Rectangle(1, 2, 3, 4)
+        self.assertEqual("[Square] (4) 2/3 - 1", str(r_string))
+
+    def test_display1_mthd(self):
+        """"
+        Testing the display method prints as expected
+        """
+        R = Rectangle(2, 2, 0, 0, 1)
+        with redirect_stdout(io.StringIO()) as buffer_io:
+            R.display()
+            out = buffer_io.getvalue()
+            self.assertEqual(out, ('#' * 2 + '\n') * 2)
+
+    def test_update_args(self):
+        """
+        Testing update method(args).confirming it updates as expected
+        """
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(89)
+        self.assertEqual(89, r1.id)
+        r1.update(89, 2)
+        self.assertEqual(2, r1.width)
+        r1.update(89, 2, 3)
+        self.assertEqual(3, r1.height)
+        r1.update(89, 2, 3, 4)
+        self.assertEqual(4, r1.x)
+        r1.update(89, 2, 3, 4, 5)
+        self.assertEqual(5, r1.y)
+
+    def test_update_kwargs(self):
+        """
+        Testing update method(kwargs).confirming it updates as expected
+        """
+        r1 = Rectangle(10, 10, 10, 10, 5)
+        r1.update(height=1)
+        self.assertEqual(1, r1.height)
+        r1.update(width=1, x=2)
+        self.assertEqual([1, 2], [r1.width, r1.x])
+        r1.update(y=1, width=2, x=3, id=89)
+        self.assertEqual([1, 2, 3, 89], [r1.y, r1.width, r1.x, r1.id])
+        r1.update(x=1, height=2, y=3, width=4)
+        self.assertEqual([1, 2, 3, 4], [r1.x, r1.height, r1.y, r1.width])
+        r1.update(x=9, y=10)
+        self.assertEqual([9, 10], [r1.x, r1.y])
