0a7a2f26cfbf3a2e597f66fa1c616d27cd463b13
rungene <rungenesamuel@gmail.com>
2023-02-16 09:11:52 -0800

    Update the class Base by adding the static method def to_json_string(list_dictionaries): that returns the JSON string representation of list_dictionaries:

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@ Base class module
 Created on Monday 13.02.2023
 @author: Rungene
 """
+import json
 
 
 class Base:
@@ -27,3 +28,18 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        static method to_json_sting - converts
+        an object to json representation
+
+        Attributes:
+            list_dictionaries -  is a list of dictionaries
+        Return:
+            he JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
