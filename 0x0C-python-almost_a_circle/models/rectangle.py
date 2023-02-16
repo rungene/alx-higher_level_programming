bab28eedda6a198263ddedb305832c3d6bc7d6d9
rungene <rungenesamuel@gmail.com>
2023-02-16 02:31:40 -0800

    Adding to_dictionary method. The Rectangle instance to dictionary representation, task 13

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -197,3 +197,13 @@ class Rectangle(Base):
                     self.x = v
                 elif k == 'y':
                     self.y = v
+
+    def to_dictionary(self):
+        """
+        public method to_dictionary
+
+        Return:
+            returns the dictionary representation of a Rectangle
+        """
+        return {'id': self.id, 'width': self.width, 'height': self.height,
+                'x': self.x, 'y': self.y}
