0136f0998248a209e5970ac76beccc9d80c1a3d7
rungene <rungenesamuel@gmail.com>
2023-03-15 19:25:17 -0700

    Improving readability. Addding spaces

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,14 +5,14 @@ CREATE TABLE IF NOT EXISTS second_table
 name VARCHAR(256),
 score INT);
 -- DML query to insert first row in table
-INSERT INTO second_table(id,name,score)
+INSERT INTO second_table (id, name, score)
 VALUES (1'John',10);
 -- DML query to insert second row in table
-INSERT INTO second_table(id,name,score)
+INSERT INTO second_table (id, name, score)
 VALUES (2,'Alex',3);
 -- DML query to insert third row in table
-INSERT INTO second_table(id,name,score)
+INSERT INTO second_table (id, name, score)
 VALUES (3,'Bob',14);
--- DML query to insert fouth row in table
-INSERT INTO second_table(id,name,score)
+-- DML query to insert fourth row in table
+INSERT INTO second_table (id, name, score)
 VALUES (4,'George',8);
