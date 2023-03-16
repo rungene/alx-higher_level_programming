03bdcf424c58fc6c28ef14edaecf5ccaf599c77e
rungene <rungenesamuel@gmail.com>
2023-03-15 18:39:09 -0700

    Write a script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- displays the number of records with id = specific using COUNT
+-- DML query to display number of records of the table with specific id
+SELECT COUNT(*) FROM first_table WHERE id=89;
