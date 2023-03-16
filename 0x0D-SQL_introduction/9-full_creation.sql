de3fb701ebc7358f2fcf0d776138ec12b4e3603e
rungene <rungenesamuel@gmail.com>
2023-03-15 19:15:00 -0700

    Optimisation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,18 @@
--- create a table using CREATE insert values using INSERT
--- DDL query to create a table and insert values
-CREATE TABLE IF NOT EXISTS second_table (id INT,name VARCHAR(256),score INT) INSERT INTO second_table(id,name,score)VALUES(1'John',10),
-(2,'Alex',3),(3,'Bob',14),(4,'George',8);
+-- create a table using CREATE
+-- DDL query to create a table if dose not exists
+CREATE TABLE IF NOT EXISTS second_table
+(id INT,
+name VARCHAR(256),
+score INT)
+-- DML query to insert first row in table
+INSERT INTO second_table(id,name,score)
+VALUES (1'John',10);
+-- DML query to insert second row in table
+INSERT INTO second_table(id,name,score)
+VALUES (2,'Alex',3);
+-- DML query to insert third row in table
+INSERT INTO second_table(id,name,score)
+VALUES (3,'Bob',14);
+-- DML query to insert fouth row in table
+INSERT INTO second_table(id,name,score)
+VALUES (4,'George',8);
