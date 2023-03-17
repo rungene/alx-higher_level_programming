c403ee8d05195b518c09747ec1ba16b7f28e9b28
rungene <rungenesamuel@gmail.com>
2023-03-16 19:36:04 -0700

    Corrections

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,7 @@
--- create a database if not exists
+-- create a database and table if not exists
 -- DDL query
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- create a table in MYSQL server states
--- DDL query to create a table and with attributes: id and name. id INT unique, auto generated, can’t be null and is a primary key
 CREATE TABLE IF NOT EXISTS states (
 id INT NOT NULL AUTO_INCREMENT UNIQUE,
 name VARCHAR(256) NOT NULL,
