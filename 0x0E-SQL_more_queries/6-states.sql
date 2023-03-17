9b45cceb240ef2786e4fb470c59d13c3ff980a30
rungene <rungenesamuel@gmail.com>
2023-03-16 19:37:46 -0700

    Corrections

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,10 +1,10 @@
--- create a database and table if not exists
--- DDL query
+-- Create database and table
+-- DDL query 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- create a table in MYSQL server states
-CREATE TABLE IF NOT EXISTS states (
+-- set db to new db
+USE hbtn_0d_usa;
+-- create new table
+CREATE TABLE IF NOT EXISTS states(
 id INT NOT NULL AUTO_INCREMENT UNIQUE,
-name VARCHAR(256) NOT NULL,
-PRIMARY KEY (id)
-);
+name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
 Create database and table
