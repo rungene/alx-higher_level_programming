fcba0d8433653b34c7196ad4e11fd40291935664
rungene <rungenesamuel@gmail.com>
2023-03-16 18:07:20 -0700

    Corrections

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- DDL Query to create MySql user with password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- adding all privileges on your MySQL server to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
