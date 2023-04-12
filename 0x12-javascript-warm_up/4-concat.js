3b09f2c729b5185fe32ac202fddd5ca1a22074a9
rungene <rungenesamuel@gmail.com>
2023-04-12 01:08:43 +0000

    Adding a semicolon

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -8,5 +8,5 @@ if (args[0] && args[1]) {
 } else if (args[0] && !args[1]) {
   console.log(`${args[0]} is undefined`);
 } else {
-  console.log('undefined is undefined')
+  console.log('undefined is undefined');
 }
