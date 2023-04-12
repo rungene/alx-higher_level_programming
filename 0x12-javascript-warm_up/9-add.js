192da1daac773e4207cc4fda92fecc280077fdc1
rungene <rungenesamuel@gmail.com>
2023-04-12 09:17:44 +0000

    Taking input from command line convert into ints perform additions

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 
-function add(a, b) {
-  let res = a + b;
+function add (a, b) {
+  const res = a + b;
   return res;
 }
-// getting third command line args
-const arg = process.argv[2];
-const arg2 = process.argv[3];
 
-// check if arg was int
-if (!arg || !arg2) {
-  console.log('NaN');
-  process.exit(0)
-}
-add(arg, arg2);
+// getting third command line args
+const arg = parseInt(process.argv[2]);
+const arg1 = parseInt(process.argv[3]);
+
+// additions and loging
+const addition = add(arg, arg1);
+console.log(addition);
