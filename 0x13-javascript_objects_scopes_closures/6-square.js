370af6f165f3cf24c77f9981eb29821e85f7a3a5
rungene <rungenesamuel@gmail.com>
2023-04-13 10:18:54 +0000

    Corrections. Reformating charPrint method

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,11 +3,17 @@
 const sqr = require('./5-square');
 
 class Square extends sqr {
-  charPrint (c) {
-    if (!c) {
-      c = 'X';
+  // defaults to the character 'X' if it is not provided.
+  charPrint (c = 'X') {
+    let row = '';
+
+    for (let i = 0; i < this.width; i++) {
+      row += c;
     }
-    this.print(c);
+
+    for (let j = 0; j < this.height; j++) {
+      console.log(row);
+    }
   }
 }
 module.exports = Square;
