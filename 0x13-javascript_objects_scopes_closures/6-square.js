8820c1088fc5bf1ca6eef2e361beacea64d656b1
rungene <rungenesamuel@gmail.com>
2023-04-13 02:55:29 +0000

    Add an instance method to print rectangle

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const sqr = require('./5-square');
+
+class Square extends sqr {
+  charPrint(c) {
+    if (!c) {
+      c = 'X'  
+    }
+    this.print(c);
+  }
+}
+module.exports = Square;
