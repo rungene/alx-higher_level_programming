a2680659b06dae9e5204419fc82c8a4db4974556
rungene <rungenesamuel@gmail.com>
2023-04-16 08:48:59 -0700

    a script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+
+author: Clevers Rungene
+"""
+import MySQLdb
+import sys
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 4:
+        print("Required: {} username password and db name".format(arg[0]))
+        exit(1)
+    # get MyQL username, databasename and password as commandline args
+    username = args[1]
+    password = args[2]
+    dbname = args[3]
+    # connect to mysql sever on localhos, port 3306
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=dbname, port=3306)
+    # cursor object
+    cursor = db.cursor()
+    # sql query to retive all states in tabel
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    # fetch the rows iterate over them and print
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    # close the cursor and db connections
+    cursor.close()
+    db.close()
