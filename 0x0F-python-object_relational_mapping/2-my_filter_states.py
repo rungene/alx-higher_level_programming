81063c723e5031a5bb28e500b70122539256775f
rungene <rungenesamuel@gmail.com>
2023-04-16 10:13:41 -0700

    a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+ lists all states with a name starting with N
+ (upper N) from the database hbtn_0e_0_usa:
+
+author: Clevers Rungene
+"""
+import MySQLdb
+import sys
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 5:
+        print("Required: {} username password, db name ".format(arg[0]))
+        exit(1)
+    # get MyQL username, databasename and password as commandline args
+    username = args[1]
+    password = args[2]
+    dbname = args[3]
+    state_name = args[4]
+    # connect to mysql sever on localhos, port 3306
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=dbname, port=3306)
+    # cursor object
+    cursor = db.cursor()
+    # generate a pattern for states name search
+    state_pattern = f'{state_name}%'
+    # sql query to retive all states matching a pattern
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                   ORDER BY id ASC".format(state_name))
+    # fetch the rows iterate over them and print
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    # close the cursor and db connections
+    cursor.close()
+    db.close()
