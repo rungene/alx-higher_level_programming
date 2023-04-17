a6431257667c2a1051a488b7b79cdde995130bb6
rungene <rungenesamuel@gmail.com>
2023-04-16 19:42:45 -0700

    a script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+ lists all states with a name starting with N
+ (upper N) from the database hbtn_0e_0_usa:
+
+author: Clevers Rungene
+"""
+import MySQLdb
+import sys
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 5:
+        print("Required: {} username password, db name ".format(arg[0]))
+        exit(1)
+    # get MyQL username, databasename and password as commandline args
+    username = args[1]
+    password = args[2]
+    dbname = args[3]
+    state_name = args[4]
+    # connect to mysql sever on localhos, port 3306
+    db = MySQLdb.connect(host="localhost", user=username,
+                         passwd=password, db=dbname, port=3306)
+    # cursor object
+    cursor = db.cursor()
+    # define an sql query with parameter from states name
+    query = """SELECT cities.name FROM cities INNER JOIN\
+            states ON cities.state_id = states.id WHERE states.name = %s"""
+    # execute the query with the state name parameter
+    cursor.execute(query, (state_name,))
+    # fetch the rows iterate over them and print
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    # close the cursor and db connections
+    cursor.close()
+    db.close()
