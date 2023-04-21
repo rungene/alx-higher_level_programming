ece405234d41c36e03dcc29088909fd694fe60ff
rungene <rungenesamuel@gmail.com>
2023-04-21 13:07:28 -0700

    Write a Python file similar to model_state.py named model_city.py that contains the class definition of a City.

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This modules defines a City class """
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base
+
+
+class City(Base):
+    """
+    A city class, inherits from Base.
+
+    Args:
+        Base: class factory that generates a base class for declarative models.
+
+    Attributes:
+    __tablename__:tablename
+    id:a column of integer
+    name:a column of a string
+    state_id: column of an integer
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey='states.id')
+    state = relationship(State, back_populates='cities')
