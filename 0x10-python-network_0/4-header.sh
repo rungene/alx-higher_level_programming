b44bfd6b910eb11e0b63618f311addc90c5ec0dc
rungene <rungenesamuel@gmail.com>
2023-05-02 07:51:22 -0700

    Corrections

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -H "X-School-User-Id: 98" -X GET "$1"
+curl -H "X-School-User-Id: 98" -sX GET "$1"
