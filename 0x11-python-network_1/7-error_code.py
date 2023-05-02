53cd6855212eae5a31e7fee05da9256c70d2a558
rungene <rungenesamuel@gmail.com>
2023-05-02 02:01:48 -0700

    Corrections

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -14,4 +14,5 @@ if __name__ == "__main__":
 
     if r.status_code < 400:
         print(r.text)
-    print("Error code: {}".format(r.status_code))
+    else:
+        print("Error code: {}".format(r.status_code))
