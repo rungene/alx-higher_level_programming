ae5313d0ded1dac4f08fcd60c36040035046fe04
rungene <rungenesamuel@gmail.com>
2023-06-02 02:49:02 -0700

    Corrections

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -5,7 +5,7 @@ const redHeader = $('#red_header');
 // Bind an event handler to the click event of readHeader element
 redHeader.on('click', function () {
   // Get header element
-  const header = $('header');
+  const header = $('HEADER');
 
   // Set color property of the header
   header.css('color', '#FF0000');
