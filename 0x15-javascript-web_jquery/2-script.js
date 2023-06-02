47232e9c85f89a0e2c9c3a55bb5e9b34898c00dd
rungene <rungenesamuel@gmail.com>
2023-06-02 03:00:13 -0700

    Corrections

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,12 +1,4 @@
 const $ = window.$;
-// Select tag DIV#red_header element
-const redHeader = $('DIV#red_header');
-
-// Bind an event handler to the click event of readHeader element
-redHeader.click(function () {
-  // Get header element
-  const header = $('HEADER');
-
-  // Set color property of the header
-  header.css('color', '#FF0000');
+$('DIV#red_header').click(function () {
+  $('HEADER').css('color', '#FF0000');
 });
