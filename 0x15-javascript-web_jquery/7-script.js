a44fb06e0bb0fdff73f778ff5987d6803260360b
rungene <rungenesamuel@gmail.com>
2023-06-03 04:04:18 -0700

    Corrections

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -3,7 +3,7 @@ $.ajax({
   url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
   dataType: 'json',
   success: function (response) {
-    $('DIV#character').text(response.nmae);
+    $('DIV#character').text(response.name);
   },
   error: function () {
     $('DIV#character').text('Error fetching character name');
