cae1eb229c85ad013748c57f70ef1fbe4e79d5b2
rungene <rungenesamuel@gmail.com>
2023-06-03 11:26:45 -0700

    Revert "Testing"
    This reverts commit 008557f83fd5f8f55a1b26088741d4eb8ac94375.

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,29 +1,15 @@
-function fetchMovieTitles() {
-  // Make a GET request to the SWAPI API
-  var request = new XMLHttpRequest();
-  request.open("GET", "https://swapi-api.alx-tools.com/api/films/?format=json");
-  request.onload = function() {
-    // Check if the request was successful
-    if (request.status === 200) {
-      // Parse the JSON response
-      var response = JSON.parse(request.responseText);
-      // Get the movie titles
-      var titles = response.results.map(function(movie) {
-        return movie.title;
-      });
-      // Use the JQuery API to select the HTML tag UL#list_movies
-      var element = $("#list_movies");
-      // Loop through the movie titles and add them as list items
-      titles.forEach(function(title) {
-        var listItem = $("<li>");
-        listItem.text(title);
-        element.append(listItem);
-      });
-    }
-  };
-  // Send the request
-  request.send();
-}
-
-// Call the fetchMovieTitles function
-fetchMovieTitles();
+const $ = window.$;
+$.ajax({
+  url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+  dataType: 'json',
+  success: function (response) {
+    const movies = response.results;
+    const $list = $('UL#list_movies');
+    $.each(movies, function (index, movie) {
+      $list.append('<li>' + movie.title + '</li>');
+    });
+  },
+  error: function () {
+    $('UL#list_movies').append('<li>Error when fetching Movie title</li>');
+  }
+});
