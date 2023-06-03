a5c1085fcacfa08d21663ba5635ca31e708c7102
rungene <rungenesamuel@gmail.com>
2023-06-03 12:19:24 -0700

    Revert "Tests"
    This reverts commit a68d385d6996be6659cdf6e88310e4fe25240b0c.

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,7 +1,14 @@
 const $ = window.$;
-const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
-
-$.getJSON(url, function (body) {
-  const hello = body.hello;
-  $('DIV#hello').text(hello);
+const $hello = $('DIV#hello');
+$(document).ready(function () {
+  $.ajax({
+    url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+    dataType: 'json',
+    success: function (response) {
+      $hello.text(response.hello);
+    },
+    error: function () {
+      $hello.text('Error fetching value of hello');
+    }
+  });
 });
