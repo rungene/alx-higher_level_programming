b54bd9d280d3c53311e4a909352dd6dd91fac4a3
rungene <rungenesamuel@gmail.com>
2023-06-03 04:02:15 -0700

    JavaScript script that fetches the character name from this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,11 @@
+const $ = window.$;
+$.ajax({
+  url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+  dataType: 'json',
+  success: function (response) {
+    $('DIV#character').text(response.nmae);
+  },
+  error: function () {
+    $('DIV#character').text('Error fetching character name');
+  }
+});
