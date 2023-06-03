319907051fb96f719a79160964a6dfcd2290d6cf
rungene <rungenesamuel@gmail.com>
2023-06-03 02:12:30 -0700

    Testing

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,11 +1,4 @@
 const $ = window.$;
-// Select tag DIV#add_item element
-const addItem = $('DIV#add_item');
-
-// Bind an event handler to the click event of addItem element
-addItem.click(function () {
-  // create a new li element
-  const li = ('<li>Item</li>');
-  // Append the new li element to to UL.my_list element
-  $('UL.my_list').append(li);
+$('DIV#add_item').click(function () {
+  $('UL.my_list').append('<li>Item</li>');
 });
